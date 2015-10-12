a79eebbe4e665585b2af65aa187ee3ada74a5c8e
Nate Armstrong <natearmstrong2@gmail.com>
2015-10-12 11:12:48 -0600

    Create new Cocoa Touch Framework
    Copies classes to the new framework and updates them to the latest swift
    syntax.

---
+++ CalendarView/CalendarView/CalendarView.h
@@ -0,0 +1,19 @@
+//
+//  CalendarView.h
+//  CalendarView
+//
+//  Created by Nate Armstrong on 10/12/15.
+//  Copyright © 2015 Nate Armstrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarView.
+FOUNDATION_EXPORT double CalendarViewVersionNumber;
+
+//! Project version string for CalendarView.
+FOUNDATION_EXPORT const unsigned char CalendarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarView/PublicHeader.h>
+
+
