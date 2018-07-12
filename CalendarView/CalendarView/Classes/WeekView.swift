c16a2f2d37f58e0fd666195f6e68fdb1dfdc1ebd
David Verbenyi <dverbenyi@gmail.com>
2018-07-12 11:53:12 +0100

    Updated to Swift 4

--- CalendarView/CalendarView/Classes/WeekView.swift
+++ CalendarView/CalendarView/Classes/WeekView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Nate Armstrong on 3/29/15.
 //  Copyright (c) 2015 Nate Armstrong. All rights reserved.
+//  Updated to Swift 4 by A&D Progress aka verebes (c) 2018
 //
 
 import UIKit
@@ -40,7 +41,7 @@ class WeekView: UIView {
 
   func setdown() {
     for day in days {
-      NotificationCenter.default.removeObserver(day)
+        NotificationCenter.default.removeObserver(day)
       day.removeFromSuperview()
     }
   }
@@ -50,7 +51,7 @@ class WeekView: UIView {
     var x: CGFloat = 0
     for i in 1...days.count {
       let day = days[i - 1]
-      day.frame = CGRect(x: x, y: 0, width: bounds.size.width / days.count, height: bounds.size.height)
+        day.frame = CGRect(x: x, y: 0, width: bounds.size.width / days.count, height: bounds.size.height)
       x = day.frame.maxX
     }
   }
@@ -61,7 +62,7 @@ class WeekView: UIView {
         let day = days[i]
         let dayDate = date.add(i, .Days)
         day.isToday = dayDate.isToday()
-        day.isOtherMonth = !month.isSameMonth(dayDate)
+        day.isOtherMonth = !month.isSameMonth(other: dayDate)
         day.selected = false
         day.date = dayDate
       }
